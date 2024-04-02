cd6150369ce65afd28442787ff55f8cfb3fe7d42
LEE YOU BIN <youbeene3409@naver.com>
2024-04-03 04:47:03 +0900

    [UI/#1] 로그인 화면 UI 구현

--- app/src/main/java/com/sopt/now/presentation/auth/login/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/auth/login/LoginActivity.kt
@@ -1,4 +1,13 @@
 package com.sopt.now.presentation.auth.login
 
-class LoginActivity {
+import android.os.Bundle
+import com.sopt.now.R
+import com.sopt.now.databinding.ActivityLoginBinding
+import com.sopt.now.util.base.BaseActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
--- app/src/main/java/com/sopt/now/presentation/auth/signup/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/auth/signup/SignUpActivity.kt
@@ -1,4 +1,13 @@
 package com.sopt.now.presentation.auth.signup
 
-class SignUpActivity {
+import android.os.Bundle
+import com.sopt.now.R
+import com.sopt.now.databinding.ActivitySignUpBinding
+import com.sopt.now.util.base.BaseActivity
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
