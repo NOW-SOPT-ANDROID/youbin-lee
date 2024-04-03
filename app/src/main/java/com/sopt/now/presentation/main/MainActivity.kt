846af1e26bc04d0030a2b776f33241e1883b9750
LEE YOU BIN <youbeene3409@naver.com>
2024-04-03 09:05:33 +0900

    [FEAT/#1] 메인 화면 구현

--- app/src/main/java/com/sopt/now/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/main/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.sopt.now.presentation.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.R
+import com.sopt.now.data.User
+import com.sopt.now.databinding.ActivityMainBinding
+import com.sopt.now.presentation.auth.login.LoginActivity.Companion.USER
+import com.sopt.now.util.base.BaseActivity
+import com.sopt.now.util.extension.getParcelable
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private lateinit var user: User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        getUserData()
+        setUserData()
     }
+
+    private fun getUserData() {
+        user = intent.getParcelable(USER, User::class.java) ?: return
+    }
+
+    private fun setUserData() {
+        with(binding) {
+            tvMyPageNickname.text = user.nickName
+            tvMyPageId.text = user.id
+            tvMyPageMbti.text = user.mbti
+        }
+    }
+
 }
