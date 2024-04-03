ea561adffe0502e25eefa05fe25f22c8ae3356d7
LEE YOU BIN <youbeene3409@naver.com>
2024-04-03 17:17:12 +0900

    [UI/#2] 로그인 화면 UI 구현

--- app/src/main/java/com/sopt/now/compose/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.sopt.now.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,16 +47,47 @@ fun LoginScreen() {
     var password by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 40.dp)
     ) {
+        Spacer(modifier = Modifier.padding(20.dp))
         Text(
-            modifier = Modifier.padding(10.dp),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             text = "Welcome To SOPT",
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold
+            fontSize = 25.sp
         )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = "ID")
+        TextField(
+            value = userId,
+            onValueChange = { userId = it },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("사용자 이름 입력") }
+        )
+        Spacer(modifier = Modifier.padding(vertical = 20.dp))
+        Text("비밀번호")
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("비밀번호 입력") }
+        )
+        Spacer(modifier = Modifier.weight(2f))
+        Button(
+            onClick = { /* 클릭 시 수행될 동작 */ },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text("로그인 하기")
+        }
+        Spacer(modifier = Modifier.padding(vertical = 5.dp))
+        Button(
+            onClick = { /* 클릭 시 수행될 동작 */ },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text("회원가입 하기")
+        }
+        Spacer(modifier = Modifier.padding(bottom = 30.dp))
     }
 }
 
