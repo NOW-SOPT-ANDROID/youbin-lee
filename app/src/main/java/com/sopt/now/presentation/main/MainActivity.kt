dec07432599f3a1ccd3dc2462ffc2fb8d205f7ab
LEE YOU BIN <youbeene3409@naver.com>
2024-04-14 21:55:00 +0900

    [MOVE/#9] MainActivity -> MyPageFragment 파일 이동

--- app/src/main/java/com/sopt/now/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/main/MainActivity.kt
@@ -2,32 +2,21 @@ package com.sopt.now.presentation.main
 
 import android.os.Bundle
 import com.sopt.now.R
-import com.sopt.now.data.User
 import com.sopt.now.databinding.ActivityMainBinding
-import com.sopt.now.presentation.auth.login.LoginActivity.Companion.USER
+import com.sopt.now.presentation.main.mypage.MyPageFragment
 import com.sopt.now.util.base.BaseActivity
-import com.sopt.now.util.extension.getParcelable
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private lateinit var user: User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        getUserData()
-        setUserData()
-    }
-
-    private fun getUserData() {
-        user = intent.getParcelable(USER, User::class.java) ?: return
-    }
-
-    private fun setUserData() {
-        with(binding) {
-            tvMyPageNickname.text = user.nickname
-            tvMyPageId.text = user.id
-            tvMyPageMbti.text = user.mbti
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvMain.id)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fcvMain.id, MyPageFragment())
+                .commit()
         }
     }
 
--- app/src/main/java/com/sopt/now/presentation/main/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sopt/now/presentation/main/mypage/MyPageFragment.kt
@@ -1,8 +1,32 @@
 package com.sopt.now.presentation.main.mypage
 
+import android.os.Bundle
+import android.view.View
 import com.sopt.now.R
+import com.sopt.now.data.User
 import com.sopt.now.databinding.FragmentMyPageBinding
 import com.sopt.now.util.base.BaseFragment
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    private lateinit var user: User
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        getUserData()
+//        setUserData()
+    }
+
+//    private fun getUserData() {
+//        user = intent.getParcelable(LoginActivity.USER, User::class.java) ?: return
+//    }
+
+//    private fun setUserData() {
+//        with(binding) {
+//            tvMyPageNickname.text = user.nickname
+//            tvMyPageId.text = user.id
+//            tvMyPageMbti.text = user.mbti
+//        }
+//    }
 }
