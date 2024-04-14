98615d34535c11f0de52bc29182bf8375959e624
LEE YOU BIN <youbeene3409@naver.com>
2024-04-14 22:03:31 +0900

    [ADD/#9] Icon 추가

--- app/src/main/java/com/sopt/now/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/presentation/main/home/HomeFragment.kt
@@ -1,9 +1,15 @@
 package com.sopt.now.presentation.main.home
 
+import android.os.Bundle
+import android.view.View
 import com.sopt.now.R
 import com.sopt.now.databinding.FragmentHomeBinding
 import com.sopt.now.util.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
--- app/src/main/java/com/sopt/now/presentation/main/search/SearchFragment.kt
+++ app/src/main/java/com/sopt/now/presentation/main/search/SearchFragment.kt
@@ -1,8 +1,14 @@
 package com.sopt.now.presentation.main.search
 
+import android.os.Bundle
+import android.view.View
 import com.sopt.now.R
 import com.sopt.now.databinding.FragmentSearchBinding
 import com.sopt.now.util.base.BaseFragment
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
