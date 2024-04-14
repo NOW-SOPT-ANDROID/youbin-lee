cbd3a37a42e75628bf08412d2ba6efc37419e6fd
LEE YOU BIN <youbeene3409@naver.com>
2024-04-15 02:14:20 +0900

    [FEAT/#9] coil 적용

--- app/src/main/java/com/sopt/now/presentation/main/home/MyProfileViewHolder.kt
+++ app/src/main/java/com/sopt/now/presentation/main/home/MyProfileViewHolder.kt
@@ -1,6 +1,7 @@
 package com.sopt.now.presentation.main.home
 
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.sopt.now.data.FriendInfo
 import com.sopt.now.databinding.ItemMyProfileBinding
 
@@ -8,7 +9,7 @@ class MyProfileViewHolder(private val binding: ItemMyProfileBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(friendData: FriendInfo.MyProfile) {
         with(binding) {
-            ivProfile.setImageResource(friendData.profileImage)
+            ivProfile.load(friendData.profileImage)
             tvName.text = friendData.name
         }
     }
