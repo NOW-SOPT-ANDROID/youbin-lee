8ce75df14d5ed3df7cf5d8276a85e76758250d0c
LEE YOU BIN <youbeene3409@naver.com>
2024-05-24 13:26:05 +0900

    [FIX/#14] Timber 및 야간모드 추가

--- app/src/main/java/com/sopt/now/MyApp.kt
+++ app/src/main/java/com/sopt/now/MyApp.kt
@@ -1,8 +1,26 @@
 package com.sopt.now
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.sopt.now.compose.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+        setDayMode()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setDayMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
 }
